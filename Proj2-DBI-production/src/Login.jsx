813820b5d161e03f359ab1d79991e0442bc603f6
JDgomez <87883400+JDgomez2002@users.noreply.github.com>
2023-03-31 00:23:44 -0600

    login

--- Proj2-DBI-production/src/Login.jsx
+++ Proj2-DBI-production/src/Login.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from 'react'
 import { supabase } from './client'
 import './Login.css'
 
-function get_login() {
+function Login() {
   const [users, setUsers] = useState([])
   const [user, setPost] = useState({patient_dpi: "", name: ""} )
   const { patient_dpi, name } = user
-
+  
   useEffect(() => {
     fetchPosts()
     console.log('database copy')
@@ -18,35 +18,11 @@ function get_login() {
     .from('patient')
     .select()
     setUsers(data)
-    // setPost(data)
     console.log("data: ", data)
     console.log('users: ', {users})
-  }
-}
-
-function Login() {
-  // get_login()
-  const [users, setUsers] = useState([])
-  const [user, setPost] = useState({patient_dpi: "", name: ""} )
-  const { patient_dpi, name } = user
-
-  useEffect(() => {
-    fetchPosts()
-    console.log('database copy')
-    console.log({users})
-  },[])
-
-  async function fetchPosts(){
-    const { data } = await supabase
-    .from('patient')
-    .select()
-    const { users } = await supabase
-    .from('patient')
-    .select()
-    setUsers(data)
-    // setPost(data)
-    console.log("data: ", data)
-    console.log('users: ', users[0])
+    if(users.length>0){
+      console.log('The USER 0: ',users[0])
+    }
   }
   return (
     //<div className="login-root" >
@@ -56,9 +32,9 @@ function Login() {
           src="https://cdn.pixabay.com/photo/2017/05/15/23/47/stethoscope-icon-2316460_960_720.png"
         />
         <div className="login-labels" >Username:</div>
-        <input></input>
+        <input onClick={fetchPosts} ></input>
         <div className="login-labels" >Password:</div>
-        <input></input>
+        <input onClick={fetchPosts} ></input>
         {
           users.map(user =>(
             <div key={user.patient_dpi}>
