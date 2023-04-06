850ff0df5e6c94942be1e21a55ff244e2085a973
Melissa Pérez <per21385@uvg.edu.gt>
2023-04-05 23:56:13 -0600

    Incidence UI and part of backend

--- Proj2-DBI-production/src/pages/Incidence/Incidence.jsx
+++ Proj2-DBI-production/src/pages/Incidence/Incidence.jsx
@@ -1,30 +1,61 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../../client'
-import { Switch, BrowserRouter, Link, Route, useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import './Incidence.css'
 
 function Incidence(){
+    const [incidence, setUsers] = useState([])
+    const history = useHistory()
+
+    const mostrarExpediente = () => {
+        if(dpiFound){
+            document.getElementById('status').textContent = 'DPI found'
+        } else {
+            document.getElementById('status').textContent = 'DPI not found'
+            document.getElementById('status').style.color = 'red'
+        }
+    }
+
+    async function fetchPosts() {
+        const { data } = await supabase.from('incidence').select()
+        setUsers(data)
+    }
+
+    const buscarExpediente = () => {
+        dpi = document.getElementById('input-dpi').value
+        dpiFound = false
+        
+        let while_counter = 0
+        if(dpi == incidence[while_counter].patient.dpi){
+            dpiFound = true
+        } else {
+            while_counter++
+        }
+        mostrarExpediente()
+        console.log('expediente', dpiFound.toString())
+    }
 
     return (
-        <div className='container'>
-            <h3>Expediente del paciente</h3>
-            <form id='paciente' action='' method='post'>
-                <label className='infoPaciente'>Ingrese la información del paciente:</label>
+        <div className='contenedor'>
+            <form id='dpi_paciente' action='' method='post'>
+                <h3>Expediente del paciente</h3>
                 <fieldset>
-                    <input placeholder='DPI del paciente' type='text' tabIndex='1' required autoFocus/>
+                    <input id="input-dpi" onClick={fetchPosts} placeholder='DPI del paciente' type='text' tabIndex='1' required autoFocus/>
                 </fieldset>
                 <fieldset>
-                    <button name='Buscar' type='submit'>Buscar</button>
+                    <button className="buscar" onClick={buscarExpediente}>Buscar</button>
+                    <div id="status" className="status-message"></div>
                 </fieldset>
-
-                <label>No. expediente: </label>
-                <label>DPI del paciente: </label>
-                <label>Enfermedades padecidas: </label>
-                {/*divididos por fecha de atención*/}
-                <label>Examenes realizados: </label>
-                <label>Hospitales visitados: </label>
-                <label>Doctores que han atendido: </label>
             </form>
+
+            <div className='detalles'>
+                <h4>No. expediente: </h4>
+                <h4>Fecha: </h4>
+                <h4>Enfermedades padecidas: </h4>
+                <h4>Examenes realizados: </h4>
+                <h4>Hospitales visitados: </h4>
+                <h4>Doctores que le han atendido: </h4>
+            </div>
         </div>
     )
 }
