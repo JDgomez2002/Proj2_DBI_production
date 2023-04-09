8684aeb0c8e472fa5b6bb7126aba67e9b9865abd
Melissa Pérez <per21385@uvg.edu.gt>
2023-04-09 00:35:55 -0600

    dpi search

--- Proj2-DBI-production/src/pages/Incidence/Incidence.jsx
+++ Proj2-DBI-production/src/pages/Incidence/Incidence.jsx
@@ -4,11 +4,44 @@ import { useHistory } from 'react-router-dom'
 import './Incidence.css'
 
 function Incidence(){
-    const [incidence, setUsers] = useState([])
+    const [incidences, setDPIs] = useState([])
+    const [incidence, setDPI] = useState({dpis: '', busqueda: false})
+    let dpiFound = false
     const history = useHistory()
 
-    const mostrarExpediente = () => {
+    useEffect(() => {
+        window.localStorage.setItem('STATUS', JSON.stringify(incidence))
+    }, [incidence])
+
+    useEffect(() => {
+    }, [incidences])
+
+    async function fetchPosts() {
+        const { data } = await supabase
+            .from('incidences')
+            .select()
+        setDPIs(data)
+    }
+
+    const buscarDPI = () => {
+        dpi = document.getElementById('input-dpi').value
+        dpiFound = false
+        
+        let while_counter = 0
+        while ((while_counter < incidences.length) && (dpiFound == false)){
+            if(dpi == incidences[while_counter].dpis){
+                dpiFound = true
+                setDPI({dpis: dpi, busqueda: true})
+            } else {
+                while_counter++
+            }
+        }
+        DPIfindMessage()
+    }
+
+    const DPIfindMessage = () => {
         if(dpiFound){
+            document.getElementById('status').style.color = 'green'
             document.getElementById('status').textContent = 'DPI found'
         } else {
             document.getElementById('status').textContent = 'DPI not found'
@@ -16,45 +49,37 @@ function Incidence(){
         }
     }
 
-    async function fetchPosts() {
-        const { data } = await supabase.from('incidence').select()
-        setUsers(data)
-    }
-
-    const buscarExpediente = () => {
-        dpi = document.getElementById('input-dpi').value
-        dpiFound = false
-        
-        let while_counter = 0
-        if(dpi == incidence[while_counter].patient.dpi){
-            dpiFound = true
-        } else {
-            while_counter++
-        }
-        mostrarExpediente()
-        console.log('expediente', dpiFound.toString())
-    }
-
     return (
         <div className='contenedor'>
             <form id='dpi_paciente' action='' method='post'>
                 <h3>Expediente del paciente</h3>
                 <fieldset>
-                    <input id="input-dpi" onClick={fetchPosts} placeholder='DPI del paciente' type='text' tabIndex='1' required autoFocus/>
-                </fieldset>
-                <fieldset>
-                    <button className="buscar" onClick={buscarExpediente}>Buscar</button>
+                    <input id="input-dpi" onClick={fetchPosts} placeholder='DPI del paciente' type='text'></input>
+                    <button className="buscar" onClick={buscarDPI}>Buscar</button>
                     <div id="status" className="status-message"></div>
                 </fieldset>
             </form>
 
             <div className='detalles'>
-                <h4>No. expediente: </h4>
-                <h4>Fecha: </h4>
-                <h4>Enfermedades padecidas: </h4>
-                <h4>Examenes realizados: </h4>
-                <h4>Hospitales visitados: </h4>
-                <h4>Doctores que le han atendido: </h4>
+                <fieldset>
+                    <h4>No. expediente: </h4>
+                    <div id="noExpediente" className="cadaDetalle"></div>
+
+                    <h4>Fecha: </h4>
+                    <div id="fecha" className="cadaDetalle"></div>
+
+                    <h4>Enfermedades padecidas: </h4>
+                    <div id="enfermedades" className="cadaDetalle"></div>
+
+                    <h4>Examenes realizados: </h4>
+                    <div id="examenes" className="cadaDetalle"></div>
+
+                    <h4>Hospitales visitados: </h4>
+                    <div id="hospitales" className="cadaDetalle"></div>
+
+                    <h4>Doctores que le han atendido: </h4>
+                    <div id="doctores" className="cadaDetalle"></div>
+                </fieldset>
             </div>
         </div>
     )
